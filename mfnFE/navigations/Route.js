1ae79fb624d0598d94678de2dd71747f3adf267c
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-12 22:00:45 +0100

    Route

--- mfnFE/navigations/Route.js
+++ mfnFE/navigations/Route.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "react-native";
 import { CredentialsContext } from "../components/CredentialsContext";
 import Auth from "./Auth";
 import Pages from "./Pages";
+import OnboardingScreen from "../components/Onboarding";
 
 
 const Stack = createNativeStackNavigator();
 
-export default function Routes() {
-    const { storedCredentials, setStoredCredentials } = React.useContext(CredentialsContext);
-    const [initialRoute, setInitialRoute] = React.useState("Auth");
-    const [loading, setLoading] = React.useState(true);
+function Routes() {
+    return(
+        <CredentialsContext.Consumer>
+            {({ storedCredentials, setStoredCredentials }) => (
+                <NavigationContainer>
+                    <StatusBar barStyle="dark-content" />
+                    <Stack.Navigator
+                        initialRouteName={storedCredentials ? "Pages" : "Onboarding"}
+                        screenOptions={{
+                            headerShown: false
+                        }}
+                    >
+                        <Stack.Screen
+                            name="Onboarding"
+                            component={OnboardingScreen}
+                            options={{
+                                headerShown: false
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Auth"
+                            component={Auth}
+                            options={{
+                                headerShown: false
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Pages"
+                            component={Pages}
+                            options={{
+                                headerShown: false
+                            }}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            )}
+    </CredentialsContext.Consumer>
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            if (storedCredentials) {
-                setInitialRoute("Pages");
-            }
-            setLoading(false);
-        }, 1000);
-    }, []);
-
-    if (loading) {
-        return null;
-    }
-
-    return (
-        <NavigationContainer>
-            <StatusBar barStyle="dark-content" />
-            <Stack.Navigator initialRouteName={initialRoute}>
-                <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
-                <Stack.Screen name="Pages" component={Pages} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
     )
 }
+
+export default Routes;
