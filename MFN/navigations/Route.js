cf895b5a75c086d9670cc12c51c0605aecc576f9
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-14 22:22:25 +0100

    update Route

--- MFN/navigations/Route.js
+++ MFN/navigations/Route.js
@@ -7,49 +7,82 @@ import Auth from "./Auth";
 import Pages from "./Pages";
 // import OnboardingScreen from "../components/Onboarding";
 import Landing from "../pages/Landing";
+import Home from "../pages/Home";
 
 
 const Stack = createNativeStackNavigator();
 
 function Routes() {
-    return(
+    return (
         <CredentialsContext.Consumer>
-            {({ storedCredentials, setStoredCredentials }) => (
+            {({ storedCredentials }) => (
                 <NavigationContainer>
                     <StatusBar barStyle="dark-content" />
                     <Stack.Navigator
-                        initialRouteName={storedCredentials ? "Pages" : "Onboarding"}
-                        screenOptions={{
-                            headerShown: false
-                        }}
-                    >
-                        <Stack.Screen
-                            name="Landing"
-                            component={Landing}
-                            options={{
-                                headerShown: false
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Auth"
-                            component={Auth}
-                            options={{
-                                headerShown: false
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Pages"
-                            component={Pages}
-                            options={{
-                                headerShown: false
-                            }}
-                        />
+                        screenOptions={{ headerShown: false }}>
+                        {storedCredentials ? (
+                            <Stack.Screen
+                                name="Pages"
+                                component={Pages}
+                            />
+                        ) : (
+                            <>
+                                <Stack.Screen
+                                    name="Landing"
+                                    component={Landing}
+                                />
+                                <Stack.Screen
+                                    name="Auth"
+                                    component={Auth}
+                                    options={{
+                                        headerShown: false
+                                    }}
+                                />
+                                {!storedCredentials && (
+
+
+                                    <Stack.Screen
+                                        name="Pages"
+                                        component={Pages}
+                                    />
+                                )}
+                            </>
+                        )}
                     </Stack.Navigator>
                 </NavigationContainer>
             )}
-    </CredentialsContext.Consumer>
-
+        </CredentialsContext.Consumer>
     )
 }
 
 export default Routes;
+//                         <Stack.Screen
+//                             name="Landing"
+//                             component={Landing}
+//                             options={{
+//                                 headerShown: false
+//                             }}
+//                         />
+//                         <Stack.Screen
+//                             name="Auth"
+//                             component={Auth}
+//                             options={{
+//                                 headerShown: false
+//                             }}
+//                         />
+//                         <Stack.Screen
+//                             name="Pages"
+//                             component={Pages}
+//                             options={{
+//                                 headerShown: false
+//                             }}
+//                         />
+//                     </Stack.Navigator>
+//                 </NavigationContainer>
+//             )}
+//         </CredentialsContext.Consumer>
+
+//     )
+// }
+
+// export default Routes;
