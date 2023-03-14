5d163e2b5c80765e05b0b795c0f43a1e0776f911
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-03-14 22:22:04 +0100

    Add Tabs Navigat

--- MFN/navigations/Pages.js
+++ MFN/navigations/Pages.js
@@ -3,40 +3,74 @@ import React from "react";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Landing from "../pages/Landing";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from 'react-native-vector-icons/MaterialIcons';
 
 
+const Tab = createMaterialBottomTabNavigator();
 const PageStack = createNativeStackNavigator();
 
 function Pages() {
     return (
-        <PageStack.Navigator
+        <Tab.Navigator
             initialRouteName="Home"
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <PageStack.Screen
-                name="Landing"
-                component={Landing}
-                options={{
-                    headerShown: false
-                }}
-            />
-            {/* <PageStack.Screen
+            activeColor="#fff"
+            barStyle={{ backgroundColor: '#000' }}
+            screenOptions=
+            {({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = focused
+                            ? 'home'
+                            : 
+                            'home';
+                    } else if (route.name === 'Profile') {
+                        iconName = focused ? 'manage_accounts' : 'manage_accounts';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+                labelStyle: { paddingBottom: 10, fontSize: 10 },
+                style: { padding: 10, height: 40 }
+            }}>
+            <Tab.Screen
                 name="Home"
                 component={Home}
                 options={{
                     headerShown: true
                 }}
-            /> */}
-            <PageStack.Screen
+            />
+            <Tab.Screen
                 name="Profile"
                 component={Profile}
                 options={{
                     headerShown: true
                 }}
             />
-        </PageStack.Navigator>
+            
+            {/* <PageStack.Screen
+                name="Landing"
+                component={Landing}
+            />
+            <PageStack.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    headerShown: true
+                }}
+            />
+            <PageStack.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    headerShown: true
+                }}
+            /> */}
+        </Tab.Navigator>
     )
 }
 
